f735f44f93a6cbf83314226eb6e2bea73fd1acdc
glebkalachev <megas781@gmail.com>
2020-01-24 13:37:06 +0300

    adaptivity added to the main page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,17 @@ export class HeaderComponent implements OnInit {
 
   constructor() { }
 
+  theWindow: Window;
+  shouldHideMobileNav: boolean = false;
   ngOnInit() {
+    this.theWindow = window;
+    console.log('thewindow: ' + window);
+  }
+
+  burgerButtonTapped() {
+    this.shouldHideMobileNav = !this.shouldHideMobileNav;
+    // console.log('window.innerWidth:');
+    // console.log(window.innerWidth);
   }
 
 }
