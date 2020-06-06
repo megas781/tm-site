abe7fec31d13ed97d1e847e34c7aa29361ebdcd9
glebkalachev <megas781@gmail.com>
2020-06-06 23:55:40 +0300

    added comments for Nastya

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,7 +1,5 @@
-import {AfterViewInit, Component, OnInit, Output} from '@angular/core';
-import {TweenLite, TimelineLite} from 'gsap';
-import Swiper from 'swiper';
-import {ScrollMagic, Scene} from 'scrollmagiclib';
+import {AfterViewInit, Component, OnInit } from '@angular/core';
+import {ScrollMagic} from 'scrollmagiclib'; //ремарка: принимаем тот факт, что ScrollMagic – это и есть класс контроллера.
 
 @Component({
   selector: 'app-main-page',
@@ -10,62 +8,18 @@ import {ScrollMagic, Scene} from 'scrollmagiclib';
 })
 export class MainPageComponent implements OnInit, AfterViewInit {
 
-  ctrl: ScrollMagic;
+  scrollCtrl: ScrollMagic;
 
   constructor() {
   }
 
   ngOnInit() {
-
+    /* Контроллер инициализируется в ngOnInit, чтобы при инициализации представления, когда мы его будем
+    передавать в awards-section (см. main-page.component.html), это свойство уже существовало */
+    this.scrollCtrl = new ScrollMagic('body');
   }
 
   ngAfterViewInit(): void {
-
-    this.ctrl = new ScrollMagic('body');
-
-    //анимации
-    // new ScrollMagic.Scene({
-    //   triggerElement: '.slogan-part__body',
-    //   triggerHook: 0
-    // })
-    //   .setTween(TweenLite.from(document.querySelector('.slogan-part__body'), 1, {y: 50, opacity: 0}))
-    //   .addTo(this.scrollController);
-    //
-    //
-    // TweenLite.from(document.querySelector('.intro__image'), 3, {opacity: 0, scale: 0.9, delay: 1.0});
-    // console.log('scrollmagic');
-
-    const scene = new Scene(window.innerHeight * 2 + 400, -200, '.awards-section', '1', true);
-
-    const silhouetteMoveLength = 50;
-    const tweenTopLeft = TweenLite.fromTo('.decoration-silhouette.top-left', 1, {
-      x: silhouetteMoveLength,
-      y: -silhouetteMoveLength
-    }, {x: -silhouetteMoveLength, y: silhouetteMoveLength, ease: 'linear'});
-    const tweenBottomLeft = TweenLite.fromTo('.decoration-silhouette.bottom-left', 1, {
-      x: -silhouetteMoveLength,
-      y: -silhouetteMoveLength
-    }, {x: silhouetteMoveLength, y: silhouetteMoveLength, ease: 'linear'});
-    const tweenBottomRight = TweenLite.fromTo('.decoration-silhouette.bottom-right', 1, {
-      x: -silhouetteMoveLength,
-      y: silhouetteMoveLength
-    }, {x: silhouetteMoveLength, y: -silhouetteMoveLength, ease: 'linear'});
-    const tweenTopRight = TweenLite.fromTo('.decoration-silhouette.top-right', 1, {
-      x: silhouetteMoveLength,
-      y: silhouetteMoveLength
-    }, {x: -silhouetteMoveLength, y: -silhouetteMoveLength, ease: 'linear'});
-
-    const awardsSilhouetteAnimation = new TimelineLite()
-      .addLabel('start', 0)
-      .add(tweenTopLeft, 'start')
-      .add(tweenBottomLeft, 'start')
-      .add(tweenBottomRight, 'start')
-      .add(tweenTopRight, 'start');
-
-    scene
-      // .AddIndicators('Awards Scene')
-      .setTween(awardsSilhouetteAnimation);
-    this.ctrl.AddScenes([scene]);
 
   }
 
