6be1fe3432f837e4ed9795eb6bf5b20d83761762
glebkalachev <megas781@gmail.com>
2020-06-15 22:13:06 +0300

    children-plays: pagination implemented

--- src/app/main-page/children-plays-section/children-plays-section.component.ts
+++ src/app/main-page/children-plays-section/children-plays-section.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import Swiper from 'swiper';
 import {ScrollMagic} from 'scrollmagiclib';
 
@@ -7,29 +7,55 @@ import {ScrollMagic} from 'scrollmagiclib';
   templateUrl: './children-plays-section.component.html',
   styleUrls: ['./children-plays-section.component.css']
 })
-export class ChildrenPlaysSectionComponent implements OnInit {
+export class ChildrenPlaysSectionComponent implements OnInit, AfterViewInit {
 
   @Input() scrollCtrl: ScrollMagic;
+
+  paginationBullets: NodeListOf<HTMLElement>;
+  activeBulletIndex: number;
+  childrenPlaySwiper: Swiper;
 
   constructor() { }
 
   ngOnInit() {
-    var childrenPlaySwiper = new Swiper('.children-plays__swiper', {
+    let self = this;
+    this.childrenPlaySwiper = new Swiper('.children-plays__swiper', {
       effect: 'coverflow',
       grabCursor: true,
       centeredSlides: true,
       slidesPerView: 'auto',
-      // coverflowEffect: {
-      //   rotate: 50,
-      //   stretch: 0,
-      //   depth: 100,
-      //   modifier: 1,
-      //   slideShadows : true,
-      // },
       pagination: {
-        el: '.children-plays__swiper .swiper-pagination',
+        el: '.years__container',
+        type: "custom",
+        bulletClass: "years__year",
+        bulletActiveClass: "active",
+        clickable: true
       },
+      autoplay: {
+        delay: 3000
+      }
     });
+    this.childrenPlaySwiper.on('slideChange', function() {
+      self.updateActiveBullet(self.childrenPlaySwiper.activeIndex);
+    });
+  }
+
+  ngAfterViewInit() {
+    this.paginationBullets = document.querySelectorAll('.years .years__year');
+
+    // this.activeBulletIndex = this.paginationBullets.length - 1;
+    // this.childrenPlaySwiper.slideTo(this.activeBulletIndex, 0);
+    this.activeBulletIndex = 0;
+    this.childrenPlaySwiper.slideTo(0, 0);
+
+    this.paginationBullets[this.activeBulletIndex].classList.add('active');
+  }
+
+  //non-zero-based
+  updateActiveBullet(newActiveIndex: number) {
+    this.paginationBullets[this.activeBulletIndex].classList.remove('active');
+    this.paginationBullets[newActiveIndex].classList.add('active');
+    this.activeBulletIndex = newActiveIndex;
   }
 
 }
