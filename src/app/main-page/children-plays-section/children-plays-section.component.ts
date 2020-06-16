d4ae8596c142b9c6d9654459cafdcb9528504fb0
glebkalachev <megas781@gmail.com>
2020-06-16 09:10:39 +0300

    children-plays: proper images set and some layout fixes

--- src/app/main-page/children-plays-section/children-plays-section.component.ts
+++ src/app/main-page/children-plays-section/children-plays-section.component.ts
@@ -15,9 +15,58 @@ export class ChildrenPlaysSectionComponent implements OnInit, AfterViewInit {
   activeBulletIndex: number;
   childrenPlaySwiper: Swiper;
 
+  plays = [
+    {
+      title: "Новогодние приключения Маши и Вити",
+      year: "2012",
+      localPath: "../../assets/images/images-by-nastya/sec-5-2012.jpg"
+    },
+    {
+      title: "Старая старая сказка",
+      year: "2013",
+      localPath: "../../assets/images/images-by-nastya/sec-5-2013.jpg"
+    },
+    {
+      title: "Бременские музыканты - 2014",
+      year: "2014",
+      localPath: "../../assets/images/images-by-nastya/sec-5-2014.jpg"
+    },
+    {
+      title: "Приключения Буратино",
+      year: "2015",
+      localPath: "../../assets/images/images-by-nastya/sec-5-2015.jpg"
+    },
+    {
+      title: "Волшебный Голос Джельсомино",
+      year: "2016",
+      localPath: "../../assets/images/images-by-nastya/sec-5-2016.jpg"
+    },
+    {
+      title: "Красная шапочка",
+      year: "2017",
+      localPath: "../../assets/images/images-by-nastya/sec-5-2017.jpg"
+    },
+    {
+      title: "Мэри Поппинс",
+      year: "2018",
+      localPath: "../../assets/images/images-by-nastya/sec-5-2018.jpg"
+    },
+    {
+      title: "Бременские музыканты - 2019",
+      year: "2019",
+      localPath: "../../assets/images/images-by-nastya/sec-5-2019.jpg"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
+    //Ставим индекс заранее, чтобы
+    this.activeBulletIndex = 0;
+  }
+
+  ngAfterViewInit() {
+
     let self = this;
     this.childrenPlaySwiper = new Swiper('.children-plays__swiper', {
       effect: 'coverflow',
@@ -38,16 +87,9 @@ export class ChildrenPlaysSectionComponent implements OnInit, AfterViewInit {
     this.childrenPlaySwiper.on('slideChange', function() {
       self.updateActiveBullet(self.childrenPlaySwiper.activeIndex);
     });
-  }
 
-  ngAfterViewInit() {
     this.paginationBullets = document.querySelectorAll('.years .years__year');
-
-    // this.activeBulletIndex = this.paginationBullets.length - 1;
-    // this.childrenPlaySwiper.slideTo(this.activeBulletIndex, 0);
-    this.activeBulletIndex = 0;
     this.childrenPlaySwiper.slideTo(0, 0);
-
     this.paginationBullets[this.activeBulletIndex].classList.add('active');
   }
 
