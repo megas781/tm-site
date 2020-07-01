c8ce8dfda0e97bf182814ddc2cadad6e093accac
glebkalachev <megas781@gmail.com>
2020-07-01 18:30:28 +0300

    mobile menu redesigned

--- src/app/master/header/header.component.ts
+++ src/app/master/header/header.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import {animate, keyframes, style, transition, trigger} from '@angular/animations';
+import {Component, OnInit} from '@angular/core';
+import {TimelineLite, TweenLite} from 'gsap';
+import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
   animations: [
-    trigger('header',[
-
+    trigger('header', [
       transition(':enter', animate('1.0s', keyframes([
         style({opacity: 0}),
         style({opacity: 1})
@@ -17,17 +17,34 @@ import {animate, keyframes, style, transition, trigger} from '@angular/animation
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   theWindow: Window;
   shouldHideMobileNav: boolean = true;
+
   ngOnInit() {
     this.theWindow = window;
     console.log('thewindow: ' + window);
   }
 
   burgerButtonTapped() {
-    this.shouldHideMobileNav = !this.shouldHideMobileNav;
+    this.shouldHideMobileNav = false;
+    let mobTimeline = new TimelineLite();
+    let links = document.querySelectorAll('.nav *:not(:last-child)');
+    const duration = 0.3;
+    const positionConst = 0.02;
+    for (let i = 1; i <= links.length; i++) {
+      mobTimeline.add(TweenLite.from(`.nav > *:nth-child(${i})`, duration, {
+        x: 100,
+        opacity: 0
+      }), positionConst * i);
+    }
+  }
+
+  closeButtonTapped() {
+    this.shouldHideMobileNav = true;
+
   }
 
 }
