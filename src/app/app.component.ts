597fb308d67fd9ab6339bd43ed3543393eef7cf8
glebkalachev <megas781@gmail.com>
2020-07-01 10:45:37 +0300

    link colors in the header are changed. Copyright text changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { AfterViewChecked, AfterViewInit, Component } from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component, OnInit} from '@angular/core';
 import { TweenLite } from "gsap";
 import Swiper from 'swiper';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tm-site';
-  constructor() {
+export class AppComponent implements OnInit {
+  title = 'Творческая Мастерская Московского Политеха';
+  constructor(titleService: Title) {
+    titleService.setTitle(this.title);
+  }
+  ngOnInit() {
 
   }
 }
