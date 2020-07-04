f0de34cc0e8fe07f52ba5d4c3a05f6aa11906755
glebkalachev <megas781@gmail.com>
2020-07-04 15:20:51 +0300

    fixed mobile-nav artefact on changing width in desktop=>mobile view

--- src/app/master/header/header.component.ts
+++ src/app/master/header/header.component.ts
@@ -3,17 +3,22 @@ import {TimelineLite, TweenLite} from 'gsap';
 import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
 import {NavigationEnd, Router} from '@angular/router';
 
-const duration = '0.3s linear'
+const duration = '0.18s';
 const transactionIn = transition('hidden => shown', animate(duration));
 const transactionOut = transition('shown => hidden', animate(duration));
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
   animations: [
 
-
-
+    trigger('header', [
+      transition(':enter', animate('0.8s', keyframes([
+        style({opacity: 0}),
+        style({opacity: 1})
+      ])))
+    ]),
     trigger('image-logo', [
       transactionIn,
       transactionOut,
@@ -34,26 +39,40 @@ const transactionOut = transition('shown => hidden', animate(duration));
         opacity: 0,
       })),
     ]),
-
-    trigger('header', [
-      transition(':enter', animate('0.8s', keyframes([
-        style({opacity: 0}),
-        style({opacity: 1})
-      ])))
-    ]),
+    trigger('nav', [
+      state('desktop', style({
+        visibility: 'visible',
+        opacity: 1,
+      })),
+      state('mobileHidden', style({
+        visibility: 'hidden',
+        opacity: 0
+      })),
+      state('mobileVisible', style({
+        visibility: 'visible',
+        opacity: 1
+      })),
+      transition('mobileHidden => desktop', []),
+      transition('mobileVisible => desktop', []),
+      transition('mobileHidden => mobileVisible', animate(duration)),
+      transition('mobileVisible => mobileHidden', animate(duration))
+    ])
   ]
 })
+
 export class HeaderComponent implements OnInit {
 
   theWindow: Window;
-  shouldHideMobileNav: boolean = true;
+  // shouldShowMobileView: boolean = false;
+  navState = '';
 
   //костыль
   currentPath = '';
   imageLogoState = '';
   textLogoState = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
 
@@ -77,12 +96,35 @@ export class HeaderComponent implements OnInit {
     document.querySelectorAll('.nav-link').forEach(function(linkElement) {
       linkElement.addEventListener('click', function() {
         window.scrollTo({top: 0, behavior: 'auto'});
+        if (self.navState == 'mobileVisible') {
+          self.navState = 'mobileHidden';
+        }
       });
     });
+
+    let onResize = () => {
+      // console.log('onResize from: ' + this.navState);
+      if (window.innerWidth > 870) {
+        this.navState = 'desktop';
+      } else {
+        if (this.navState === '') {
+          //Первичная настройка
+          this.navState = 'mobileHidden';
+        } else if (this.navState == 'desktop') {
+          this.navState = 'mobileHidden'
+        } else if (this.navState == 'mobileVisible') {
+          //ничего не меняем
+        }
+      }
+      // console.log(`onResize after: ${this.navState}`);
+    }
+    onResize();
+    window.addEventListener('resize', onResize);
   }
 
-  burgerButtonTapped() {
-    this.shouldHideMobileNav = false;
+  onBurgerButtonClicked() {
+    // this.shouldShowMobileView = true;
+    this.navState = 'mobileVisible';
     let mobTimeline = new TimelineLite();
     let links = document.querySelectorAll('.nav *:not(:last-child)');
     const duration = 0.3;
@@ -96,8 +138,9 @@ export class HeaderComponent implements OnInit {
   }
 
   closeButtonTapped() {
-    this.shouldHideMobileNav = true;
-
+    // this.shouldShowMobileView = false;
+    // console.log(`from: ${this.navState}`);
+    this.navState = 'mobileHidden';
   }
 
 }
