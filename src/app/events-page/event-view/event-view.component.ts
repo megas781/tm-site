033000d368f7b08907b8d3811688b7fc797d1b0d
glebkalachev <megas781@gmail.com>
2020-07-04 13:28:13 +0300

    Added expand-button in mobile event-view

--- src/app/events-page/event-view/event-view.component.ts
+++ src/app/events-page/event-view/event-view.component.ts
@@ -1,17 +1,57 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {Event} from '../../data-model/Event';
+import {TweenLite} from 'gsap';
+
 @Component({
   selector: 'app-event-view',
   templateUrl: './event-view.component.html',
   styleUrls: ['./event-view.component.css']
 })
-export class EventViewComponent implements OnInit {
+export class EventViewComponent implements OnInit, AfterViewInit {
 
   @Input() event: Event;
 
-  constructor() { }
+  isTextRevealed = false;
+
+  @ViewChild('paragraphElement', {static: true}) paragraphElement: ElementRef;
+
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onRevealButtonTapped() {
+    this.isTextRevealed = !this.isTextRevealed;
+  }
+
+  isTextOverflowed() {
+
+  }
+
+  ngAfterViewInit() {
+    console.log('asdf');
+
+    let self = this;
+    let onResize = function() {
+      // console.log(self);
+
+      //Подсчет, нужен ли collapse параграфу
+      let elem = (self.paragraphElement.nativeElement as HTMLElement);
+
+      if (self.event.title == 'Спектакль "Петля времени"') {
+        console.log(elem.clientHeight);
+      }
+
+      elem.classList.remove('collapsed');
+      if (elem.clientHeight > 180) {
+        elem.classList.add('collapsed');
+      } else {
+        elem.classList.remove('collapsed');
+      }
+    }
+    onResize()
+    window.addEventListener('resize', onResize);
+  }
 }
